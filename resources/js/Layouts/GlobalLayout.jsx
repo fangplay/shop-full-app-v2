714b05410a91f75e265747077038454fae6e9d3a
fangplay <fangplay_2014@hotmail.com>
2024-01-28 03:39:58 +0700

    change nav bar update

--- resources/js/Layouts/GlobalLayout.jsx
+++ resources/js/Layouts/GlobalLayout.jsx
@@ -9,10 +9,7 @@ import {
     SquaresPlusIcon,
     XMarkIcon,
 } from "@heroicons/react/24/outline";
-import {
-    PhoneIcon,
-    PlayCircleIcon,
-} from "@heroicons/react/20/solid";
+import { PhoneIcon, PlayCircleIcon } from "@heroicons/react/20/solid";
 
 const products = [
     {
@@ -56,287 +53,35 @@ function classNames(...classes) {
 }
 
 export default function GlobalLayout() {
-    // var AuthNavbar = "";
-    // if (!localStorage.getItem("auth_token")) {
-    //     AuthNavbar = (
-    //         <>
-    //             <nav
-    //                 className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
-    //                 aria-label="Global"
-    //             >
-    //                 <div className="flex lg:flex-1">
-    //                     <a href="/shop" className="-m-1.5 p-1.5">
-    //                         <h3>NeoShop</h3>
-    //                     </a>
-    //                 </div>
-    //                 <div className="flex lg:hidden">
-    //                     <button
-    //                         type="button"
-    //                         className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-    //                         onClick={() => setMobileMenuOpen(true)}
-    //                     >
-    //                         <span className="sr-only">Open main menu</span>
-    //                         <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-    //                     </button>
-    //                 </div>
-
-    //                 <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-    //                     <nav class="ButtonGroup-sc-71xg1o-0 loacbu">
-    //                         <a
-    //                             class="Header__NavItem-sc-i83ruj-5 dfEEyh"
-    //                             href="/shop/register"
-    //                         >
-    //                             Register
-    //                         </a>
-    //                         <a
-    //                             class="Button__StyledOutboundLink-sc-fltzk0-2 xtGAq"
-    //                             href="/shop/login"
-    //                             variant="secondary"
-    //                             type="link"
-    //                         >
-    //                             <span class="Button__ButtonText-sc-fltzk0-0 joZKAe">
-    //                                 Sign In
-    //                             </span>
-    //                         </a>
-    //                     </nav>
-    //                 </div>
-    //             </nav>
-
-    //             <Dialog
-    //                 as="div"
-    //                 className="lg:hidden"
-    //                 open={mobileMenuOpen}
-    //                 onClose={setMobileMenuOpen}
-    //             >
-    //                 <div className="fixed inset-0 z-10" />
-    //                 <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-    //                     <div className="flex items-center justify-between">
-    //                         <a href="#" className="-m-1.5 p-1.5">
-    //                             <span className="sr-only">NeoShop</span>
-    //                         </a>
-    //                         <button
-    //                             type="button"
-    //                             className="-m-2.5 rounded-md p-2.5 text-gray-700"
-    //                             onClick={() => setMobileMenuOpen(false)}
-    //                         >
-    //                             <span className="sr-only">Close menu</span>
-    //                             <XMarkIcon
-    //                                 className="h-6 w-6"
-    //                                 aria-hidden="true"
-    //                             />
-    //                         </button>
-    //                     </div>
-    //                     <div className="mt-6 flow-root">
-    //                         <div className="-my-6 divide-y divide-gray-500/10">
-    //                             <div className="py-6">
-    //                                 <a
-    //                                     href="/register"
-    //                                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-    //                                 >
-    //                                     Register
-    //                                 </a>
-    //                             </div>
-    //                             <div className="py-6">
-    //                                 <a
-    //                                     href="/login"
-    //                                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-    //                                 >
-    //                                     Log in
-    //                                 </a>
-    //                             </div>
-    //                         </div>
-    //                     </div>
-    //                 </Dialog.Panel>
-    //             </Dialog>
-    //         </>
-    //     );
-    // } else {
-    //     AuthNavbar = (
-    //         <>
-    //             <nav
-    //                 className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
-    //                 aria-label="Global"
-    //             >
-    //                 <div className="flex lg:flex-1">
-    //                     <a href="/shop" className="-m-1.5 p-1.5">
-    //                         <h3>NeoShop</h3>
-    //                     </a>
-    //                 </div>
-    //                 <div className="flex lg:hidden">
-    //                     <button
-    //                         type="button"
-    //                         className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-    //                         onClick={() => setMobileMenuOpen(true)}
-    //                     >
-    //                         <span className="sr-only">Open main menu</span>
-    //                         <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-    //                     </button>
-    //                 </div>
-
-    //                 <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-    //                     <nav class="ButtonGroup-sc-71xg1o-0 loacbu">
-    //                         <a
-    //                             class="Header__NavItem-sc-i83ruj-5 dfEEyh"
-    //                             href="/user"
-    //                         >
-    //                             Profile
-    //                         </a>
-    //                         <a
-    //                             class="Button__StyledOutboundLink-sc-fltzk0-2 xtGAq"
-    //                             href="/logout"
-    //                             variant="secondary"
-    //                             type="link"
-    //                         >
-    //                             <span class="Button__ButtonText-sc-fltzk0-0 joZKAe">
-    //                                 Logout
-    //                             </span>
-    //                         </a>
-    //                     </nav>
-    //                 </div>
-    //             </nav>
-
-    //             <Dialog
-    //                 as="div"
-    //                 className="lg:hidden"
-    //                 open={mobileMenuOpen}
-    //                 onClose={setMobileMenuOpen}
-    //             >
-    //                 <div className="fixed inset-0 z-10" />
-    //                 <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-    //                     <div className="flex items-center justify-between">
-    //                         <a href="#" className="-m-1.5 p-1.5">
-    //                             <span className="sr-only">NeoShop</span>
-    //                         </a>
-    //                         <button
-    //                             type="button"
-    //                             className="-m-2.5 rounded-md p-2.5 text-gray-700"
-    //                             onClick={() => setMobileMenuOpen(false)}
-    //                         >
-    //                             <span className="sr-only">Close menu</span>
-    //                             <XMarkIcon
-    //                                 className="h-6 w-6"
-    //                                 aria-hidden="true"
-    //                             />
-    //                         </button>
-    //                     </div>
-    //                     <div className="mt-6 flow-root">
-    //                         <div className="-my-6 divide-y divide-gray-500/10">
-    //                             <div className="py-6">
-    //                                 <a
-    //                                     href="/user"
-    //                                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-    //                                 >
-    //                                     Profile
-    //                                 </a>
-    //                             </div>
-    //                             <div className="py-6">
-    //                                 <a
-    //                                     href="/logout"
-    //                                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-    //                                 >
-    //                                     Logout
-    //                                 </a>
-    //                             </div>
-    //                         </div>
-    //                     </div>
-    //                 </Dialog.Panel>
-    //             </Dialog>
-    //         </>
-    //     );
-    // }
-
-    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
     return (
         <header className="bg-white">
             {/* desktop view */}
-            <nav
-                className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
-                aria-label="Global"
-            >
-
-            <div className="flex lg:flex-1">
-                    <a href="/" className="-m-1.5 p-1.5">
-                        <h3>NeoShop</h3>
-                    </a>
-                </div>
-
-                <div className="flex lg:hidden">
-                    <button
-                        type="button"
-                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-                        onClick={() => setMobileMenuOpen(true)}
+            <nav class="bg-white border-gray-200 dark:bg-gray-900">
+                <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+                    <a
+                        href="/"
+                        class="flex items-center space-x-3 rtl:space-x-reverse"
                     >
-                        <span className="sr-only">Open main menu</span>
-                        <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-                    </button>
-                </div>
-
-                <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                    <nav class="ButtonGroup-sc-71xg1o-0 loacbu">
+                        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                            NeoShop
+                        </span>
+                    </a>
+                    <div class="flex items-center space-x-6 rtl:space-x-reverse">
                         <a
-                            class="Header__NavItem-sc-i83ruj-5 dfEEyh"
                             href="/register"
+                            class="text-sm  text-gray-500 dark:text-white hover:underline"
                         >
                             Register
                         </a>
                         <a
-                            class="Button__StyledOutboundLink-sc-fltzk0-2 xtGAq"
                             href="/login"
-                            variant="secondary"
-                            type="link"
+                            class="text-sm  text-blue-600 dark:text-blue-500 hover:underline"
                         >
-                            <span class="Button__ButtonText-sc-fltzk0-0 joZKAe">
-                                Sign In
-                            </span>
+                            Login
                         </a>
-                    </nav>
+                    </div>
                 </div>
             </nav>
-            {/* mobile view */}
-            <Dialog
-                as="div"
-                className="lg:hidden"
-                open={mobileMenuOpen}
-                onClose={setMobileMenuOpen}
-            >
-                <div className="fixed inset-0 z-10" />
-                <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-                    <div className="flex items-center justify-between">
-                        <a href="#" className="-m-1.5 p-1.5">
-                            <span className="sr-only">NeoShop</span>
-                        </a>
-                        <button
-                            type="button"
-                            className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                            onClick={() => setMobileMenuOpen(false)}
-                        >
-                            <span className="sr-only">Close menu</span>
-                            <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                        </button>
-                    </div>
-                    <div className="mt-6 flow-root">
-                        <div className="-my-6 divide-y divide-gray-500/10">
-                            <div className="py-6">
-                                <a
-                                    href="/register"
-                                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                                >
-                                    Register
-                                </a>
-                            </div>
-                            <div className="py-6">
-                                <a
-                                    href="/login"
-                                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                                >
-                                    Log in
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </Dialog.Panel>
-            </Dialog>
             {/* {AuthNavbar} */}
         </header>
     );
