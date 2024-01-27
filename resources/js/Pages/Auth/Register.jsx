f1e84daaae79352670aca67e7a95c0846134c580
fangplay <fangplay_2014@hotmail.com>
2024-01-28 05:14:51 +0700

    update register page

--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import GuestLayout from "@/Layouts/GuestLayout";
 import InputError from "@/Components/InputError";
 import InputLabel from "@/Components/InputLabel";
 import PrimaryButton from "@/Components/PrimaryButton";
@@ -194,9 +193,10 @@ export default function Register() {
                                 value="Birth Date"
                             />
 
-
+                            <div class="relative max-w-sm">
+                                <input datepicker datepicker-autohide type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="birthdate" />
+                            </div>
                         </div>
-
 
                         <div className="flex items-center justify-end mt-4">
                             <Link
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,6 +13,8 @@
         <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
         <script src="../path/to/flowbite/dist/datepicker.js"></script>
 
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/datepicker.min.js"></script>
+
         <!-- Scripts -->
         @routes
         @viteReactRefresh
